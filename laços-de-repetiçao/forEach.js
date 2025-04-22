ded051efb3d6eda4385ef1ce14e98cf41c8345c8
DanielVerissimo1 <danielSantoss1300@gmail.com>
2025-04-22 18:23:55 -0300

    feat: fazendo novas questoes e aprendendo mais sobre map

--- laços-de-repetiçao/forEach.js
+++ laços-de-repetiçao/forEach.js
@@ -1,15 +1,16 @@
-// let soma = 0
+ /*
+ let soma = 0
 
-// const numeros = [12,9,0,4,5]
-// // funçao normal
-// function minhaFuncao(item) {
-//     alert(item)
-// }
-// numeros("item01")
+ const numeros = [12,9,0,4,5]
+  funçao normal
+ function minhaFuncao(item) {
+     alert(item)
+ }
+ numeros("item01")
 
-// // forEach serve para dar funçao aos elementos 
+  forEach serve para dar funçao aos elementos 
 
-// numeros.forEach(minhaFuncao)
+ numeros.forEach(minhaFuncao)
 
 
 
@@ -18,12 +19,22 @@ const notas = [10,6.5,8,7.5]
 let somaDasNotas = 0
 
 
-// function somaNotas(nota) {
-//     somaDasNotas += nota    
-// }
-// notas.forEach(somaNotas)
+ function somaNotas(nota) {
+     somaDasNotas += nota    
+ }
+ notas.forEach(somaNotas)
 notas.forEach((nota)=>{somaDasNotas += nota})
 
 const media = somaDasNotas/ notas.length
 
-console.log(media)
+console.log(media)*/
+
+
+
+const notas = [10,6.5,8,7.5]
+
+function mostraNota(nota) {
+    console.log(nota)
+}
+
+notas.forEach(mostraNota)
